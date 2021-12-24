5b19b012a012d71b93228ad0b0de3de9d4f58485
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2021-12-24 19:45:33 +0530

    added start page

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.set("json spaces", 2);
 // single route to send mail to clients
 app.use("/mail", mailHandler);
 
+// start page
+app.get("/", (req, res) => {
+	res.status(200).json({
+		msg: "Welcome to the OrkaMailer",
+        
+	});
+});
+
 app.listen(2000, async () => {
 	console.log("Server is running on port 2000");
 });
